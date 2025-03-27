74f41a383c4addcd5d2fe9cb5948bb3051cef924
MartaBernardoZamora <martabernardozamora@gmail.com>
2025-03-27 18:06:28 +0100

    REFACTORY calculatePaymentTest from Car. Vehicule class and test changes to abstract class

--- src/test/java/dev/martabernardo/peaje/vehicle/VehicleTest.java
+++ src/test/java/dev/martabernardo/peaje/vehicle/VehicleTest.java
@@ -7,17 +7,27 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class VehicleTest {
+    static class AnyVehicle extends Vehicle {
+        public AnyVehicle(String vehicleId) {
+            super(vehicleId);
+        }
+
+        @Override
+        public int calculatePayment() {
+            return 0;
+        }
+    }
     @Test
     @DisplayName("Test getVehicleId")
     public void getVehicleIdTest() {
-        Vehicle vehicle = new Vehicle("1234MNL");
+        Vehicle vehicle = new AnyVehicle("1234MNL");
         String vehicleId = vehicle.getVehicleId();
         assertThat(vehicleId, is("1234MNL"));
     }
     @Test
     @DisplayName("Test calculatePayment")
     public void calculatePaymentTest() {
-        Vehicle vehicle = new Vehicle("1234MNL");
+        Vehicle vehicle = new AnyVehicle("1234MNL");
         int payment = vehicle.calculatePayment();
         assertThat(payment, is(0));
     }
