0c40a8fa676de2439a8999befd9c6e57e28f6621
MartaBernardoZamora <martabernardozamora@gmail.com>
2025-03-27 17:34:56 +0100

    STATUS OK getVehicleIdTest from Vehicle

---
+++ src/main/java/dev/martabernardo/peaje/vehicle/Vehicle.java
@@ -0,0 +1,20 @@
+package dev.martabernardo.peaje.vehicle;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "vehicle")
+public class Vehicle {
+    
+    private String vehicleId;
+
+    public Vehicle(String vehicleId) {
+        this.vehicleId = vehicleId;
+    }
+
+    public String getVehicleId() {
+        return vehicleId;
+    }
+
+}
