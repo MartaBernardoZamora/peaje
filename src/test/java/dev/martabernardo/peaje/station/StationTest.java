e3fa1477a5dd2f4d1ed8b081356569cf2be26666
MartaBernardoZamora <martabernardozamora@gmail.com>
2025-03-27 12:04:28 +0100

    FAILED  getCityTest from Station

--- src/test/java/dev/martabernardo/peaje/station/StationTest.java
+++ src/test/java/dev/martabernardo/peaje/station/StationTest.java
@@ -1,15 +1,31 @@
 package dev.martabernardo.peaje.station;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class StationTest {
+
+    private Station station;
+
+    @BeforeEach
+    public void setUp() {
+        station = new Station("Huerna", "Asturias");
+    }
     @Test
     @DisplayName("Test getName")
     public void getNameTest() {
-        Station station = new Station("Huerna");
         String name = station.getName();
         assert name.equals("Huerna");
+    }
+    @Test
+    @DisplayName("Test getCity")
+    public void getCityTest() {
+        String city = station.getCity();
+        assertThat(city, is("Asturias"));
     }
 
 }
