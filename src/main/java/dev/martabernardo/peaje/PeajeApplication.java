304f1f4d3dc712b7be744d074d773c8f830e651a
MartaBernardoZamora <martabernardozamora@gmail.com>
2025-03-27 11:40:27 +0100

    initial commit

---
+++ src/main/java/dev/martabernardo/peaje/PeajeApplication.java
@@ -0,0 +1,13 @@
+package dev.martabernardo.peaje;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeajeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeajeApplication.class, args);
+	}
+
+}
