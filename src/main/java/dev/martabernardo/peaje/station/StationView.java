680538683ecfc2450332fb8afd6652721a45c42d
MartaBernardoZamora <martabernardozamora@gmail.com>
2025-03-28 16:56:48 +0100

    STATUS OK printVehiclesTest from StationView

--- src/main/java/dev/martabernardo/peaje/station/StationView.java
+++ src/main/java/dev/martabernardo/peaje/station/StationView.java
@@ -1,11 +1,21 @@
 package dev.martabernardo.peaje.station;
 
+import java.util.List;
+
+import dev.martabernardo.peaje.vehicle.VehicleDTOSummary;
+
 public class StationView {
 
     public void printStation(Station station) {
         String name = station.getName();
         String city = station.getCity();
         System.out.println("Peaje " + name + " (" + city +")");        
+    }
+
+    public void printVehicles(List<VehicleDTOSummary> vehicles) {
+        for (VehicleDTOSummary vehicle : vehicles) {
+            System.out.println(vehicle.type() + "/" + vehicle.vehicleId() + "/" + vehicle.payment());
+        }
     }
 
 }
