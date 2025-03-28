b0c74a31775dcf919b2d3e324f7aaef63e0999df
MartaBernardoZamora <martabernardozamora@gmail.com>
2025-03-28 14:40:19 +0100

    STATUS OK getVehicleListWithPaymentAndType from StationService

--- src/main/java/dev/martabernardo/peaje/vehicle/VehicleDTOSummary.java
+++ src/main/java/dev/martabernardo/peaje/vehicle/VehicleDTOSummary.java
@@ -1,7 +1,10 @@
 package dev.martabernardo.peaje.vehicle;
 
-public record VehicleDTOSummary(String vehicleId, int payment) {
+public record VehicleDTOSummary(String vehicleId, int payment, String type) {
     public static VehicleDTOSummary from(Vehicle vehicle) {
-        return new VehicleDTOSummary(vehicle.getVehicleId(), vehicle.calculatePayment());
+        return new VehicleDTOSummary(
+            vehicle.getVehicleId(), 
+            vehicle.calculatePayment(), 
+            vehicle.getClass().getSimpleName());
     }
 }
