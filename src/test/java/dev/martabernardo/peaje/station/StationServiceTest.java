6f6b32fa093357486a31ff3f2e1651219687f096
MartaBernardoZamora <martabernardozamora@gmail.com>
2025-03-28 14:27:14 +0100

    REFACTORY getVehicleListWithPayment from StationService. Obsoleted test deleted

--- src/test/java/dev/martabernardo/peaje/station/StationServiceTest.java
+++ src/test/java/dev/martabernardo/peaje/station/StationServiceTest.java
@@ -8,7 +8,6 @@ import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.Set;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -19,7 +18,6 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import dev.martabernardo.peaje.vehicle.Car;
 import dev.martabernardo.peaje.vehicle.Moto;
 import dev.martabernardo.peaje.vehicle.Truck;
-import dev.martabernardo.peaje.vehicle.Vehicle;
 import dev.martabernardo.peaje.vehicle.VehicleDTOSummary;
 
 @ExtendWith(MockitoExtension.class)
@@ -27,27 +25,6 @@ public class StationServiceTest {
     @Mock
     private StationRepository stationRepository;
 
-
-    @Test
-    @DisplayName("Vehicle list")
-    void getVehicleList() {
-        StationService stationService = new StationService(stationRepository);
-
-        Station station = new Station("Peaje Norte", "Oviedo");
-        station.getVehicles().addAll(List.of(
-            new Car("CAR001"),    
-            new Moto("MOTO001"),  
-            new Truck("TRK001", 3) 
-        ));
-
-        when(stationRepository.findById(1L)).thenReturn(Optional.of(station));
-
-        Set<Vehicle> vehicles = stationService.getVehicleList(1L);
-
-        assertThat(vehicles, notNullValue());
-        assertThat(vehicles, hasSize(3));
-        verify(stationRepository).findById(1L); 
-    }
     @Test
     @DisplayName("Vehicle list with payment")
     void getVehicleListWithPayment() {
