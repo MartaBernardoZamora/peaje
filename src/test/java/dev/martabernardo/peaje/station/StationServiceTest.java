ffb3f8867e2bcce879a935ac922cb646c1717c03
MartaBernardoZamora <martabernardozamora@gmail.com>
2025-03-28 14:58:15 +0100

    FAILED getTotalPayment from StationService

--- src/test/java/dev/martabernardo/peaje/station/StationServiceTest.java
+++ src/test/java/dev/martabernardo/peaje/station/StationServiceTest.java
@@ -3,6 +3,7 @@ package dev.martabernardo.peaje.station;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.Matchers.is;
 import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -27,27 +28,6 @@ public class StationServiceTest {
     private StationRepository stationRepository;
 
     @Test
-    @DisplayName("Vehicle list with payment")
-    void getVehicleListWithPayment() {
-        StationService stationService = new StationService(stationRepository);
-
-        Station station = new Station("Peaje Norte", "Oviedo");
-        station.getVehicles().addAll(List.of(
-            new Car("CAR001"),    
-            new Moto("MOTO001"),  
-            new Truck("TRK001", 3)  
-        ));
-
-        when(stationRepository.findById(1L)).thenReturn(Optional.of(station));
-
-        List<VehicleDTOSummary> vehicles = stationService.getVehicleList(1L);
-        System.out.println(vehicles);
-
-        assertThat(vehicles, notNullValue());
-        assertThat(vehicles, hasSize(3));
-        verify(stationRepository).findById(1L); 
-    }
-    @Test
     @DisplayName("Vehicle list with payment and type")
     void getVehicleListWithPaymentAndType() {
         StationService stationService = new StationService(stationRepository);
@@ -71,6 +51,24 @@ public class StationServiceTest {
             new VehicleDTOSummary("MOTO001", 50, "Moto"),
             new VehicleDTOSummary("TRK001", 150, "Truck")
         ));
+        verify(stationRepository).findById(1L); 
+    }
+    @Test
+    @DisplayName("Test get Total payment")
+    void getTotalPaymentTest() {
+        StationService stationService = new StationService(stationRepository);
+
+        Station station = new Station("Peaje Norte", "Oviedo");
+        station.getVehicles().addAll(List.of(
+            new Car("CAR001"),      
+            new Moto("MOTO001"),  
+            new Truck("TRK001", 3)  
+        ));
+
+        when(stationRepository.findById(1L)).thenReturn(Optional.of(station));   
+
+        int totalPayment = stationService.getTotalPayment(1L);
+        assertThat(totalPayment, is(300));
         verify(stationRepository).findById(1L); 
     }
 }
