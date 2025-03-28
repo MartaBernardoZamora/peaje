30b70c9234fa7441b9f7f67d90dfd40cf3d00151
MartaBernardoZamora <martabernardozamora@gmail.com>
2025-03-28 16:46:15 +0100

    FAILED printStationTest from StationView

---
+++ src/test/java/dev/martabernardo/peaje/station/StationViewTest.java
@@ -0,0 +1,28 @@
+package dev.martabernardo.peaje.station;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class StationViewTest {
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+    @Test
+    @DisplayName("Test printStation imprime correctamente nombre y ciudad")
+    void printStationTest() {
+        StationView view = new StationView();
+        System.setOut(new PrintStream(outContent));
+        view.printStationName(new Station("Norte", "Oviedo"));
+
+        String output = outContent.toString();
+
+        assertThat(output, containsString("Norte"));
+        assertThat(output, containsString("Oviedo"));
+        System.setOut(originalOut);
+    }
+}
