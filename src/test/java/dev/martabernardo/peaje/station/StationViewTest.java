57bfe66727014534de4331272927a3d00bb4b34c
MartaBernardoZamora <martabernardozamora@gmail.com>
2025-03-28 16:56:31 +0100

    FAILED printVehiclesTest from StationView

--- src/test/java/dev/martabernardo/peaje/station/StationViewTest.java
+++ src/test/java/dev/martabernardo/peaje/station/StationViewTest.java
@@ -5,9 +5,12 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.List;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import dev.martabernardo.peaje.vehicle.VehicleDTOSummary;
 
 public class StationViewTest {
     private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
@@ -23,6 +26,18 @@ public class StationViewTest {
 
         assertThat(output, containsString("Norte"));
         assertThat(output, containsString("Oviedo"));
+        System.setOut(originalOut);
+    }
+    @Test
+    @DisplayName("Test printVehicles")
+    void printVehiclesTest() { 
+        StationView view = new StationView();
+        System.setOut(new PrintStream(outContent));
+        view.printVehicles(List.of(new VehicleDTOSummary("CAR001", 100, "Car")));
+        String output = outContent.toString();
+        assertThat(output, containsString("CAR001"));
+        assertThat(output, containsString("100"));
+        assertThat(output, containsString("Car"));
         System.setOut(originalOut);
     }
 }
