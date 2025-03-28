f420896f40c4fba77f748ebaef717966f0a251b0
MartaBernardoZamora <martabernardozamora@gmail.com>
2025-03-28 18:26:05 +0100

    FAILED indextest from StationController

--- src/test/java/dev/martabernardo/peaje/station/StationControllerTest.java
+++ src/test/java/dev/martabernardo/peaje/station/StationControllerTest.java
@@ -3,6 +3,7 @@ package dev.martabernardo.peaje.station;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.CoreMatchers.is;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
 import static org.hamcrest.collection.IsIterableContainingInAnyOrder.containsInAnyOrder;
@@ -14,6 +15,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import java.lang.reflect.Field;
 
 
 import dev.martabernardo.peaje.vehicle.VehicleDTOSummary;
@@ -22,6 +24,9 @@ public class StationControllerTest {
 
     @Mock
     StationService service;
+
+    @Mock
+    StationView view;
 
 
     @Test
@@ -55,5 +60,42 @@ public class StationControllerTest {
 
         int totalPayment = stationController.getTotalPayment(1L);
         assertThat(totalPayment, is(300));
-    }   
+    }
+    @Test
+    @DisplayName("test full report from index ")
+    void indexTest() throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
+        StationController stationController = new StationController(service, view);
+        Station station1 = new Station("Norte", "Oviedo");
+        Station station2 = new Station("Sur", "Sevilla");
+
+        List<Station> stations = List.of(station1, station2);
+
+        List<VehicleDTOSummary> vehicles1 = List.of(
+            new VehicleDTOSummary("CAR001", 100, "Car")
+        );
+
+        List<VehicleDTOSummary> vehicles2 = List.of(
+            new VehicleDTOSummary("TRK002", 200, "Truck")
+        );
+
+        when(service.getAllStations()).thenReturn(stations);
+        when(service.getVehicleList(1L)).thenReturn(vehicles1);
+        when(service.getVehicleList(2L)).thenReturn(vehicles2);
+        when(service.getTotalPayment(1L)).thenReturn(100);
+        when(service.getTotalPayment(2L)).thenReturn(200);
+        Field field = Station.class.getDeclaredField("id");
+        field.setAccessible(true);
+        field.set(station1, 1L);
+        field.set(station2, 2L);
+
+        stationController.index();
+
+        verify(view).printStation(station1);
+        verify(view).printVehicles(vehicles1);
+        verify(view).printTotalPayment(100);
+
+        verify(view).printStation(station2);
+        verify(view).printVehicles(vehicles2);
+        verify(view).printTotalPayment(200);
+    }
 }
